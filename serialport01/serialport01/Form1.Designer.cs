370662c145d7d3fe3a05278c81fec5f06db6b778
Jeong Won Cheol <sturdyegg@gmail.com>
2014-05-08 09:59:59 +0900

    Rx성공

--- serialport01/serialport01/Form1.Designer.cs
+++ serialport01/serialport01/Form1.Designer.cs
@@ -28,86 +28,149 @@
 	/// </summary>
 	private void InitializeComponent()
 	{
-			this.components = new System.ComponentModel.Container();
-			this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
-			this.comboBox1 = new System.Windows.Forms.ComboBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.button2 = new System.Windows.Forms.Button();
-			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.checkBox1 = new System.Windows.Forms.CheckBox();
-			this.SuspendLayout();
-			// 
-			// comboBox1
-			// 
-			this.comboBox1.FormattingEnabled = true;
-			this.comboBox1.Location = new System.Drawing.Point(12, 12);
-			this.comboBox1.Name = "comboBox1";
-			this.comboBox1.Size = new System.Drawing.Size(121, 20);
-			this.comboBox1.TabIndex = 0;
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(139, 12);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "open";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(12, 38);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(121, 21);
-			this.textBox1.TabIndex = 2;
-			// 
-			// button2
-			// 
-			this.button2.Location = new System.Drawing.Point(140, 38);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(75, 23);
-			this.button2.TabIndex = 3;
-			this.button2.Text = "send";
-			this.button2.UseVisualStyleBackColor = true;
-			this.button2.Click += new System.EventHandler(this.button2_Click);
-			// 
-			// textBox2
-			// 
-			this.textBox2.Location = new System.Drawing.Point(12, 105);
-			this.textBox2.Multiline = true;
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(202, 185);
-			this.textBox2.TabIndex = 4;
-			// 
-			// checkBox1
-			// 
-			this.checkBox1.AutoSize = true;
-			this.checkBox1.Location = new System.Drawing.Point(13, 83);
-			this.checkBox1.Name = "checkBox1";
-			this.checkBox1.Size = new System.Drawing.Size(46, 16);
-			this.checkBox1.TabIndex = 5;
-			this.checkBox1.Text = "Hex";
-			this.checkBox1.UseVisualStyleBackColor = true;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(458, 302);
-			this.Controls.Add(this.checkBox1);
-			this.Controls.Add(this.textBox2);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.comboBox1);
-			this.Name = "Form1";
-			this.Text = "serial port";
-			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
-			this.Load += new System.EventHandler(this.Form1_Load);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+	        this.components = new System.ComponentModel.Container();
+	        this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+	        this.comboBox1 = new System.Windows.Forms.ComboBox();
+	        this.button1 = new System.Windows.Forms.Button();
+	        this.textBox1 = new System.Windows.Forms.TextBox();
+	        this.button2 = new System.Windows.Forms.Button();
+	        this.textBox2 = new System.Windows.Forms.TextBox();
+	        this.checkBox1 = new System.Windows.Forms.CheckBox();
+	        this.btnclose = new System.Windows.Forms.Button();
+	        this.btnAPStart = new System.Windows.Forms.Button();
+	        this.btnAPStop = new System.Windows.Forms.Button();
+	        this.btnBGPolling = new System.Windows.Forms.Button();
+	        this.btnACC = new System.Windows.Forms.Button();
+	        this.SuspendLayout();
+	        // 
+	        // comboBox1
+	        // 
+	        this.comboBox1.FormattingEnabled = true;
+	        this.comboBox1.Location = new System.Drawing.Point(13, 14);
+	        this.comboBox1.Name = "comboBox1";
+	        this.comboBox1.Size = new System.Drawing.Size(121, 20);
+	        this.comboBox1.TabIndex = 0;
+	        // 
+	        // button1
+	        // 
+	        this.button1.Location = new System.Drawing.Point(139, 12);
+	        this.button1.Name = "button1";
+	        this.button1.Size = new System.Drawing.Size(75, 23);
+	        this.button1.TabIndex = 1;
+	        this.button1.Text = "open";
+	        this.button1.UseVisualStyleBackColor = true;
+	        this.button1.Click += new System.EventHandler(this.button1_Click);
+	        // 
+	        // textBox1
+	        // 
+	        this.textBox1.Location = new System.Drawing.Point(12, 70);
+	        this.textBox1.Name = "textBox1";
+	        this.textBox1.Size = new System.Drawing.Size(283, 21);
+	        this.textBox1.TabIndex = 2;
+	        // 
+	        // button2
+	        // 
+	        this.button2.Location = new System.Drawing.Point(302, 70);
+	        this.button2.Name = "button2";
+	        this.button2.Size = new System.Drawing.Size(75, 23);
+	        this.button2.TabIndex = 3;
+	        this.button2.Text = "send";
+	        this.button2.UseVisualStyleBackColor = true;
+	        this.button2.Click += new System.EventHandler(this.button2_Click);
+	        // 
+	        // textBox2
+	        // 
+	        this.textBox2.Location = new System.Drawing.Point(12, 97);
+	        this.textBox2.Multiline = true;
+	        this.textBox2.Name = "textBox2";
+	        this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+	        this.textBox2.Size = new System.Drawing.Size(365, 274);
+	        this.textBox2.TabIndex = 4;
+	        // 
+	        // checkBox1
+	        // 
+	        this.checkBox1.AutoSize = true;
+	        this.checkBox1.Checked = true;
+	        this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+	        this.checkBox1.Location = new System.Drawing.Point(13, 45);
+	        this.checkBox1.Name = "checkBox1";
+	        this.checkBox1.Size = new System.Drawing.Size(46, 16);
+	        this.checkBox1.TabIndex = 5;
+	        this.checkBox1.Text = "Hex";
+	        this.checkBox1.UseVisualStyleBackColor = true;
+	        // 
+	        // btnclose
+	        // 
+	        this.btnclose.Location = new System.Drawing.Point(139, 41);
+	        this.btnclose.Name = "btnclose";
+	        this.btnclose.Size = new System.Drawing.Size(75, 23);
+	        this.btnclose.TabIndex = 6;
+	        this.btnclose.Text = "close";
+	        this.btnclose.UseVisualStyleBackColor = true;
+	        this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+	        // 
+	        // btnAPStart
+	        // 
+	        this.btnAPStart.Location = new System.Drawing.Point(221, 12);
+	        this.btnAPStart.Name = "btnAPStart";
+	        this.btnAPStart.Size = new System.Drawing.Size(75, 23);
+	        this.btnAPStart.TabIndex = 7;
+	        this.btnAPStart.Text = "AP Start";
+	        this.btnAPStart.UseVisualStyleBackColor = true;
+	        this.btnAPStart.Click += new System.EventHandler(this.btnAPStart_Click);
+	        // 
+	        // btnAPStop
+	        // 
+	        this.btnAPStop.Location = new System.Drawing.Point(221, 41);
+	        this.btnAPStop.Name = "btnAPStop";
+	        this.btnAPStop.Size = new System.Drawing.Size(75, 23);
+	        this.btnAPStop.TabIndex = 8;
+	        this.btnAPStop.Text = "AP Stop";
+	        this.btnAPStop.UseVisualStyleBackColor = true;
+	        this.btnAPStop.Click += new System.EventHandler(this.btnAPStop_Click);
+	        // 
+	        // btnBGPolling
+	        // 
+	        this.btnBGPolling.Location = new System.Drawing.Point(302, 14);
+	        this.btnBGPolling.Name = "btnBGPolling";
+	        this.btnBGPolling.Size = new System.Drawing.Size(75, 23);
+	        this.btnBGPolling.TabIndex = 9;
+	        this.btnBGPolling.Text = "BG Polling";
+	        this.btnBGPolling.UseVisualStyleBackColor = true;
+	        this.btnBGPolling.Click += new System.EventHandler(this.btnBGPolling_Click);
+	        // 
+	        // btnACC
+	        // 
+	        this.btnACC.Location = new System.Drawing.Point(302, 41);
+	        this.btnACC.Name = "btnACC";
+	        this.btnACC.Size = new System.Drawing.Size(75, 23);
+	        this.btnACC.TabIndex = 10;
+	        this.btnACC.Text = "ACC";
+	        this.btnACC.UseVisualStyleBackColor = true;
+	        this.btnACC.Click += new System.EventHandler(this.btnACC_Click);
+	        // 
+	        // Form1
+	        // 
+	        this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+	        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+	        this.ClientSize = new System.Drawing.Size(387, 383);
+	        this.Controls.Add(this.btnACC);
+	        this.Controls.Add(this.btnBGPolling);
+	        this.Controls.Add(this.btnAPStop);
+	        this.Controls.Add(this.btnAPStart);
+	        this.Controls.Add(this.btnclose);
+	        this.Controls.Add(this.checkBox1);
+	        this.Controls.Add(this.textBox2);
+	        this.Controls.Add(this.button2);
+	        this.Controls.Add(this.textBox1);
+	        this.Controls.Add(this.button1);
+	        this.Controls.Add(this.comboBox1);
+	        this.Name = "Form1";
+	        this.Text = "ez430 serialport";
+	        this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+	        this.Load += new System.EventHandler(this.Form1_Load);
+	        this.ResumeLayout(false);
+	        this.PerformLayout();
 
 	}
 
@@ -120,6 +183,11 @@
 	private System.Windows.Forms.Button button2;
 	private System.Windows.Forms.TextBox textBox2;
 	private System.Windows.Forms.CheckBox checkBox1;
+	private System.Windows.Forms.Button btnclose;
+	private System.Windows.Forms.Button btnAPStart;
+	private System.Windows.Forms.Button btnAPStop;
+	private System.Windows.Forms.Button btnBGPolling;
+	private System.Windows.Forms.Button btnACC;
         }
 }
 
--- serialport01/serialport01/Form1.cs
+++ serialport01/serialport01/Form1.cs
@@ -11,100 +11,132 @@ using System.IO.Ports;			        //시리얼포트를 위한
 
 namespace serialport01
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-			serialPort1.DataReceived += new SerialDataReceivedEventHandler(EventDataReceived);
-		}
-
-		private void Form1_Load(object sender, EventArgs e)
-		{
-			find_serial();
-		}
-
-
-
-		private void find_serial()		        //시리얼 포트 몽땅 검색해서 콤포박스에 넣음
-		{
-			string[] portsArray = SerialPort.GetPortNames();
-			foreach (string portnumber in portsArray)
-			{
-				comboBox1.Items.Add(portnumber);
-			}
-		}
-
-		private void Open_serial()
-		{
-			if (serialPort1.IsOpen) serialPort1.Close();
-
-			serialPort1.PortName = comboBox1.Text;
-			serialPort1.BaudRate = 115200;
-			serialPort1.Parity = Parity.None;
-			serialPort1.DataBits = 8;
-			serialPort1.StopBits = StopBits.One;
-			serialPort1.Open();
-		}
-
-		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
-		{
-			serialPort1.Close();
-		}
-
-		private void button1_Click(object sender, EventArgs e)
-		{
-			
-			Open_serial();
-			//byte[] buffer_send = new byte[1];
-			//buffer_send[1] = 0xff;
-			//serialPort1.Write(buffer_send, 0, 1);
-
-		}
-
-		private void button2_Click(object sender, EventArgs e)
-		{
-			// HEXA 로 보낼때 필요한 변수
-			byte[] byteSendData = new byte[200];
-			int iSendCount = 0;  // 헥사로 보낼때의 데이터 수
-			try
-			{
-				foreach (string s in textBox1.Text.Split(' '))
-				{
-					if (null != s && "" != s)
-						byteSendData[iSendCount++] = Convert.ToByte(s, 16);
-				}
-				serialPort1.Write(byteSendData, 0, iSendCount);
-
-			}
-			catch (System.Exception ex)
-			{
-				MessageBox.Show(ex.Message, "SEND 데이터 오류");
-			}
-		}
-		 void EventDataReceived(object sender, SerialDataReceivedEventArgs e)
+        public partial class Form1 : Form
         {
-			CheckForIllegalCrossThreadCalls = false;
-            int iRecSize = serialPort1.BytesToRead; // 수신된 데이터 갯수
-            string strRxData;
-
-            if (iRecSize != 0) // 수신된 데이터의 수가 0이 아닐때만 처리하자
-            {
-                strRxData = "";
-                byte[] buff = new byte[iRecSize];
-
-                serialPort1.Read(buff, 0, iRecSize);
-
-                for (int iTemp = 0; iTemp < iRecSize; iTemp++)
-                  foreach (byte bData in buff)
-                   {
-                    if (checkBox1.Checked)
-                        strRxData += " " + buff[iTemp].ToString("X2");
-                    else
-                        strRxData += Convert.ToChar(buff[iTemp]);
-                }
-                textBox2.Text += strRxData;
-            }
-        }
+	public Form1()
+	{
+	        InitializeComponent();
+	        serialPort1.DataReceived += new SerialDataReceivedEventHandler(EventDataReceived);
 	}
+
+	private void Form1_Load(object sender, EventArgs e)
+	{
+	        find_serial();
+	}
+
+
+
+	private void find_serial()		        //시리얼 포트 몽땅 검색해서 콤포박스에 넣음
+	{
+	        string[] portsArray = SerialPort.GetPortNames();
+	        foreach (string portnumber in portsArray)
+	        {
+		comboBox1.Items.Add(portnumber);
+	        }
+	        comboBox1.SelectedIndex = 1;
+	}
+
+	private void Open_serial()
+	{
+	        if (serialPort1.IsOpen) serialPort1.Close();
+
+	        serialPort1.PortName = comboBox1.Text;
+	        serialPort1.BaudRate = 115200;
+	        serialPort1.Parity = Parity.None;
+	        serialPort1.DataBits = 8;
+	        serialPort1.StopBits = StopBits.One;
+	        serialPort1.Open();
+	}
+
+	private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+	{
+	        serialPort1.Close();
+	}
+
+	private void button1_Click(object sender, EventArgs e)
+	{
+	        if(comboBox1.Text == "")
+	        {
+		MessageBox.Show("자네 포트를 설정하지 않았다네...");
+	        }
+	        else 
+		Open_serial();
+	        //byte[] buffer_send = new byte[1];
+	        //buffer_send[1] = 0xff;
+	        //serialPort1.Write(buffer_send, 0, 1);
+
+	}
+
+	private void button2_Click(object sender, EventArgs e)
+	{
+	        sendmsg(textBox1.Text);
+	}
+	void EventDataReceived(object sender, SerialDataReceivedEventArgs e)
+	{
+	        CheckForIllegalCrossThreadCalls = false;	//보안에러 무시용 구문
+	        int iRecSize = serialPort1.BytesToRead;		//수신 버퍼에 있는 바이트 수 가져옴
+	        string strRxData;
+
+	        if (iRecSize != 0)				// 수신된 데이터의 수가 0이 아닐때만 처리하자
+	        {
+		strRxData = "";
+		byte[] buff = new byte[iRecSize];
+
+		serialPort1.Read(buff, 0, iRecSize);
+
+		for (int iTemp = 0; iTemp < iRecSize; iTemp++)
+		        if (checkBox1.Checked)
+			strRxData += " " + buff[iTemp].ToString("X2");
+		        else
+			strRxData += Convert.ToChar(buff[iTemp]);
+		textBox2.Text += strRxData;
+		textBox2.AppendText("\r\n");
+	        }
+	}
+
+	private void btnclose_Click(object sender, EventArgs e)
+	{
+	        serialPort1.Close();
+	}
+
+	private void btnAPStart_Click(object sender, EventArgs e)
+	{
+	        sendmsg("ff 07 03");
+	}
+
+	private void btnAPStop_Click(object sender, EventArgs e)
+	{
+	        sendmsg("ff 09 03");
+	}
+	
+	private void sendmsg(string msg)
+	{
+	        byte[] byteSendData = new byte[200];
+	        int sendcnt = 0;						         // 헥사로 보낼때의 데이터 수
+	        try
+	        {
+		foreach (string s in msg.Split(' '))
+		{
+		        if (null != s && "" != s)
+			byteSendData[sendcnt++] = Convert.ToByte(s, 16);	        //스트링s를 16진수 바이트로 차곡차곡
+		}
+		serialPort1.Write(byteSendData, 0, sendcnt);
+	        }
+	        catch (System.Exception ex)
+	        {
+		MessageBox.Show(ex.Message, "SEND 데이터 오류");
+	        }
+	}
+
+	private void btnBGPolling_Click(object sender, EventArgs e)
+	{
+	        sendmsg("ff 20 07 00 00 00 00");
+	}
+
+	private void btnACC_Click(object sender, EventArgs e)
+	{
+	        sendmsg("ff 08 07 00 00 00 00");
+	        
+	}
+        }
 }
